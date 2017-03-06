ec4a072e60b4cfd2f69a836ee514678c65a6667d
Michael Lewis <lewismj@kahui.io>
2017-03-06 13:55:48 +0800

    try to fix travis build issue

--- travisTestRunner.sh
+++ travisTestRunner.sh
@@ -1,5 +1,1 @@
 cd ./cucumber-plugin; sbt ++$TRAVIS_SCALA_VERSION clean test
-cd ../cucumber-plugin-example; sbt ++$TRAVIS_SCALA_VERSION clean test
-cd ../cucumber-runner; sbt ++$TRAVIS_SCALA_VERSION clean test
-cd ../cucumber-runner-example; sbt ++$TRAVIS_SCALA_VERSION clean test
-
